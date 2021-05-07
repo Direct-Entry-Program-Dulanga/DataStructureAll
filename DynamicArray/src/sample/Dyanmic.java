fd61c7031ca2154ab9a54b7b21160db89ad00a46
Dulanga Nimesha <idulanganimesha@gmail.com>
2021-05-07 11:59:36 +0530

    add DynamicArray Add commit

--- DynamicArray/src/sample/Dyanmic.java
+++ DynamicArray/src/sample/Dyanmic.java
@@ -3,13 +3,31 @@ package sample;
 public class Dyanmic {
     private int[] arrayDyanmic;
 
-    public void add(int number){
-
+    public void add1(int number){
+        if(arrayDyanmic == null){
+            arrayDyanmic = new int[1];
+            arrayDyanmic[0] = number;
+        }else{
+            int[] temp = new int[arrayDyanmic.length+1];
+            for (int i = 0; i < arrayDyanmic.length; i++) {
+                temp[i] = arrayDyanmic[i];
+            }
+            temp[temp.length-1] = number;
+            arrayDyanmic = temp;
+        }
     }
 
     public void add(int index, int number){
-
+        int[] temp = new int[arrayDyanmic.length+1];
+        for (int i = 0; i < arrayDyanmic.length; i++) {
+            if(i == index){
+                temp[i] = arrayDyanmic[i];
+            }
+        }
+        temp[temp.length-1] = number;
+        arrayDyanmic = temp;
     }
+
 
     public void remove(int index){
 
--- DynamicArray/src/sample/Test5.java
+++ DynamicArray/src/sample/Test5.java
@@ -1,4 +1,10 @@
 package sample;
 
 public class Test5 {
+    public static void main(String[] args) {
+        Dyanmic dyanmic = new Dyanmic();
+        dyanmic.add1(10);
+        dyanmic.add1(12);
+        dyanmic.add(2,34);
+    }
 }
